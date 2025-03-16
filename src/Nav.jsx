45043329b0c4d61a5a16174d5a53a5859ddc8f20
Blissmal <malutibethuel@gmail.com>
2025-03-16 03:05:40 +0300

    code refactor

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,11 +1,11 @@
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Nav = () => {
   return (
-    <div className='d-flex justify-content-center py-2 shadow-sm fs-2 fw-bold'>
+    <div className="d-flex justify-content-center py-2 shadow-sm fs-2 fw-bold">
       Book Management System
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
